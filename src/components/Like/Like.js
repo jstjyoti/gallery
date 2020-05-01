b9bedc5d9a6287a1157ff823716e2f6a30b6fc32
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-01 10:10:02 +0530

    Added Like button component on Gallery

---
+++ src/components/Like/Like.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Like.css';
+
+const Like = (props) =>{
+ return  <span className="Like" onClick={props.click} style={{color: props.colored}}><i className="fa fa-heart heart" style={{fontSize:'36px'}}></i></span>
+}
+
+export default Like;
