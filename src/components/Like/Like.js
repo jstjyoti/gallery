a77105ffc2aaf7285a98548fdbd7a20f445f2806
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-01 16:55:20 +0530

    Improved style of like button

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Like.css';
 
 const Like = (props) =>{
- return  <span className="Like" onClick={props.click} style={{color: props.colored}}><i className="fa fa-heart heart" style={{fontSize:'36px'}}></i></span>
+ return  <span className="Like" onClick={props.click} style={{color: props.colored}}><i className="fa fa-heart heart" style={{fontSize:'30px'}}></i></span>
 }
 
 export default Like;
