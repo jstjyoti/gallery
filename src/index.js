2a9217f464576e48692a628f7ddec7245c7f7bd1
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-06 18:14:13 +0530

    added Browser Router for implementing routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { BrowserRouter } from 'react-router-dom';
+const app=(
+  <BrowserRouter>
+    <App></App>
+  </BrowserRouter>
+)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
