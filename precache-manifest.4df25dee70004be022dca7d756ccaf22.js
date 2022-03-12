f8747cc7896d85b0c7e5855cc52364649100dcb7
jmishra <jmishra@evertz.com>
2022-03-12 18:02:27 +0530

    Updates

---
+++ precache-manifest.4df25dee70004be022dca7d756ccaf22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb923103b946af1ef763f8d53121160",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "0f72f3457a8dbd61cabd",
+    "url": "/gallery/static/css/main.11346327.chunk.css"
+  },
+  {
+    "revision": "546fea2b05cf842e2598",
+    "url": "/gallery/static/js/2.466f823c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gallery/static/js/2.466f823c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f72f3457a8dbd61cabd",
+    "url": "/gallery/static/js/main.aadc4baf.chunk.js"
+  },
+  {
+    "revision": "1160e6796d671babbaaa",
+    "url": "/gallery/static/js/runtime-main.7b22ee28.js"
+  }
+]);
